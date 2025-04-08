61dc0dbbfa1ece3d92e7ee30a629182a506d5d8d
alanaainez <alainez649@gmail.com>
2025-04-07 22:07:07 -0400

    changed config for vite again

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,17 +1,18 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default ({ mode }) => {
-  
-  return defineConfig({
-    plugins: [react()],
-    build: {
-      outDir: "dist",
-      assetsDir: "assets",
-      emptyOutDir: true,
-      sourcemap: true,
-    },
-  });
-};
+export default defineConfig({
+  plugins: [react()],
+    server: {
+    port: 3000,
+    open: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+        secure: false,
+      },
+    }
+  }
+})
